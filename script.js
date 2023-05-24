6874f2711bcf0149942f4b38c7a69acb453ad816
Thato Mongwe <thatomongwe47@gmail.com>
2023-05-24 14:10:55 +0000

    update img link

--- script.js
+++ script.js
@@ -86,8 +86,8 @@ const projectData = [{
   badge2: 'Bootstrap',
   badge3: 'Ruby on Rails',
   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent ",
-  demoLink: 'https://johnadibe.github.io',
-  source: 'https://github.com/Johnadibe/Johnadibe.github.io',
+  demoLink: 'https://tarto-4.github.io',
+  source: 'https://github.com/Tarto-4/Tarto-4.github.io',
 
 },
 ];
