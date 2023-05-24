3407543f9f1e05b72e388f4e039dca424edc70ce
Thato Mongwe <thatomongwe47@gmail.com>
2023-05-24 11:52:31 +0000

    popup testing

--- script.js
+++ script.js
@@ -35,6 +35,52 @@ const projectData = [{
   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent ",
   demoLink: 'https://tarto-4.github.io',
   source: 'https://github.com/Tarto-4/Tarto-4.github.io',
-
 },
 ];
+
+// Works Section
+
+const worksSection = document.querySelector('.works-section');
+const postStories = document.createElement('div');
+postStories.classList = 'post-stories';
+worksSection.appendChild(postStories);
+
+const image = document.createElement('img');
+image.src = projectData[0].imageSrc;
+postStories.appendChild(image);
+
+const postTextBox = document.createElement('div');
+postTextBox.classList = 'post-text-box';
+postStories.appendChild(postTextBox);
+
+const postStoriesHeading = document.createElement('h3');
+postStoriesHeading.classList = 'post-stories-heading';
+postStoriesHeading.innerText = projectData[0].title;
+postTextBox.appendChild(postStoriesHeading);
+
+const postStoriesDescription = document.createElement('p');
+postStoriesDescription.classList = 'post-stories-description';
+postStoriesDescription.innerText = projectData[0].description;
+postTextBox.appendChild(postStoriesDescription);
+
+const worksList = document.createElement('ul');
+worksList.classList = 'works-list';
+postTextBox.appendChild(worksList);
+
+let li = document.createElement('li');
+li.innerText = projectData[0].badge1;
+worksList.appendChild(li);
+li = document.createElement('li');
+li.innerText = projectData[0].badge2;
+worksList.appendChild(li);
+li = document.createElement('li');
+li.innerText = projectData[0].badge3;
+worksList.appendChild(li);
+li = document.createElement('li');
+li.innerText = projectData[0].badge4;
+worksList.appendChild(li);
+
+let btnSmall = document.createElement('button');
+btnSmall.classList = 'btn-sm show-pop-up';
+btnSmall.innerHTML = 'See Project';
+postTextBox.appendChild(btnSmall);
