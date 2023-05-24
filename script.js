2c1fa1447d722d0044b62c43310ff8a76b14407b
Thato Mongwe <thatomongwe47@gmail.com>
2023-05-24 11:33:47 +0000

    popup test

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ navLinks.forEach((link) => {
     headerElement.classList.remove('nav-open');
   });
 });
+
+// Project and Pop up Data
+
+const projectData = [{
+  imageSrc: 'img/post-stories.svg',
+  title: 'Multi-Post Stories',
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text',
+  badge1: 'css',
+  badge2: 'Html',
+  badge3: 'bootstrap',
+  badge4: 'Ruby',
+},
+];
