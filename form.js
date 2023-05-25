6afa939c9b13da2d9c3e5bef9a932a814ae1728b
Thato Mongwe <thatomongwe47@gmail.com>
2023-05-26 00:13:00 +0200

    Update form.js

--- form.js
+++ form.js
@@ -1,6 +1,6 @@
 //  Lower case E-mail validation
 
-const form = document.querySelector('form');
+const form = document.querySelector('.contact-form padding-left-right');
 const email = document.getElementById('user_email');
 const errorMessage = document.querySelector('.error');
 
