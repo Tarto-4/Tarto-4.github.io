69a2675ff34d8e328ae439a0fc50c1e8e7464010
Thato Mongwe <thatomongwe47@gmail.com>
2023-05-25 23:51:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ navLinks.forEach((link) => {
     headerElement.classList.remove('nav-open');
   });
 });
+
+// VALIDATIONA FORM
+
+const error = document.getElementById('error-message');
+const form = document.querySelector('.contact-form');
+const userEmail = document.getElementById('user-email');
+const errorMessage = 'Please write your email address in lower case';
+// CODE
+form.addEventListener('submit', (event) => {
+  // preventing from submittion
+  event.preventDefault();
+  const userInput = userEmail.value;
+  if (userInput !== userInput.toLowerCase()) {
+    error.innerText = errorMessage;
+  } else {
+    error.innerText = '';
+    form.submit();
+  }
+});
