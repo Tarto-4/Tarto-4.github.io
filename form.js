050eba0bdc498daa707bbff89e21a9b2149a753b
Thato Mongwe <thatomongwe47@gmail.com>
2023-05-26 00:16:12 +0200

    Update form.js

--- form.js
+++ form.js
@@ -1,8 +1,9 @@
 //  Lower case E-mail validation
 
+const error = document.getElementById('error-message');
 const form = document.querySelector('.contact-form padding-left-right');
 const email = document.getElementById('user_email');
-const errorMessage = document.querySelector('.error');
+const errorMessage = 'Please write your email address in lower case';
 
 function showError() {
   if (email.value.trim() !== email.value.trim().toLowerCase()) {
